4ec0b535a127f6f5d9d9c4dbb7538c79a2a4c0ca
Adofo Stephen <adofo9433@gmail.com>
2023-06-16 15:53:58 -0700

    puts cmd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* C command using puts cmd intead of printf*/
+/* 
+ *main - This program uses the puts command to display
+ * C command using puts cmd intead of printf
+ * */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
