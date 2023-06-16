2f461c62bcd5c031f1af917e7f2fe0b32731e40a
Adofo Stephen <adofo9433@gmail.com>
2023-06-15 17:31:53 -0700

    cmd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* C command using puts cmd intead of printf*/
 int main(void) 
 {
-    puts("''Programming is like building a multilingual puzzle");
+    puts("Programming is like building a multilingual puzzle");
     return 0;
 }
