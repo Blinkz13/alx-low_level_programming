a066d3397c6bac582dc71e013a5dedce03895a15
Adofo Stephen <adofo9433@gmail.com>
2023-06-16 14:49:28 -0700

    printf cmd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /* Displaying with printf*/
-int main(void) {
-printf("With proper grammar, but the outcome is a piece of art,\n");
+int main(void) 
+{
+printf("With proper grammar, but the outcome is a piece of art,\n"); /* Printf command use */
 return (0);
 }
