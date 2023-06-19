98f87316f695900312da6420629286fe552db640
Adofo Stephen <adofo9433@gmail.com>
2023-06-19 04:49:54 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,9 @@
 int main(void)
 {
 char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, str, 59);
+int len = sizeof(str) - 1;
+write(2, str, len);
+fprintf(stderr, "(%d chars long)n", len);
+fprintf(stderr, "[stderr]: n(0 chars long)n");
 return (1);
 }
