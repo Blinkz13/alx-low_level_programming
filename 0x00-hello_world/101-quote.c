9eec19bedf9c9d46155a7515424b20afe1c1d772
Adofo Stephen <adofo9433@gmail.com>
2023-06-19 05:00:56 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2,str,59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
