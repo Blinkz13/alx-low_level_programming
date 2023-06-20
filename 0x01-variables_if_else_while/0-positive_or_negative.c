0559017ce5f357e5f1b26e928c699b7efae119d4
Adofo Stephen <adofo9433@gmail.com>
2023-06-20 05:52:45 -0700

    displaying value of a variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - displaying a number stored in the variable n
+ * and using if statment to check for conditions
+ * Retur: 0 returns no value
+ **/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d is ", n);
+	if (n > 0)
+	{
+	printf("positive\n");
+	}
+	else if (n == 0)
+	{
+	printf("zero\n");
+	}
+	else
+	{
+	printf("negative\n");
+	}
+
+	return (0);
+}
