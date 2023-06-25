f2fdce6fb6eb5a5d63688520cd7f7e0794133d67
Adofo Stephen <adofo9433@gmail.com>
2023-06-25 08:48:19 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+        printf("%dn", i);
+        }
+
+        return (0);
+}
