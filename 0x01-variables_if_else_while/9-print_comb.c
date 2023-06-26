af8495e31a7c44fdfe700c6ab006a7b98f388f00
Adofo Stephen <adofo9433@gmail.com>
2023-06-26 14:52:02 -0700

    print combo numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,17 +11,14 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = i; j < 10; j++)
-	{
 	putchar(i + '0');
-	putchar(',');
-	putchar(j + '0');
-	if (i != 9 || j != 9)
+	if (i != 9)
 	{
 	putchar(',');
 	}
 	}
-	}
-	
+
+	putchar('\n');
+
 	return (0);
 }
