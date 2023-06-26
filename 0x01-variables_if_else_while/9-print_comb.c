d88b27bbc62c7302c3f6f3a9d2fa4bd39091357b
Adofo Stephen <adofo9433@gmail.com>
2023-06-26 13:07:30 -0700

    display combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,15 +15,13 @@ int main(void)
 	{
 	putchar(i + '0');
 	putchar(',');
-	putchar(' ');
 	putchar(j + '0');
 	if (i != 9 || j != 9)
 	{
 	putchar(',');
-	putchar(' ');
 	}
 	}
 	}
-	putchar('n');
+	
 	return (0);
 }
